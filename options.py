24111da1a806353ed1991d9ab9066a0210142728
Ursuline <3534155+Ursuline@users.noreply.github.com>
2021-01-13 23:34:11 +0100

    cosmetic enhancements

--- options.py
+++ options.py
@@ -18,7 +18,7 @@ FUTURES_FLAG = True # Compute options from futures
 # Lattice parameters:
 S0   = 100.0 # initial price
 T    = 0.5 # maturity (years)
-SIG  = 0.2 # volatility
+SIG  = 0.2 # volat
 N    = 10 # number of periods
 R    = 0.02 # risk-free rate
 D    = 0.01 # dividend yield
@@ -35,14 +35,15 @@ EXPO = 10 # expiration - accomodates diff w/ #periods in lattice (EXPO<=N)
 
 class SecurityParameters:
     '''Encapsulates parameters for the underlying security'''
+    # pylint: disable=too-many-instance-attributes
 
     def __init__(self):
-        self.init       = S0
-        self.maturity   = T
-        self.volatility = SIG
-        self.n_periods  = N
-        self.rate       = R
-        self.dividend   = D
+        self.init      = S0
+        self.matur     = T
+        self.volat     = SIG
+        self.n_periods = N
+        self.rate      = R
+        self.dividend  = D
 
         self._binomial_model_parameter()
         self._risk_free_proba()
@@ -50,29 +51,28 @@ class SecurityParameters:
 
     def _binomial_model_parameter(self):
         ''' Computes binomial model parameters u & d=1/u
-        u=r_ud[0] d=r_ud[1]
-        Convert Black-Scholes / calibrate a binomial model'''
+            u=r_ud[0] d=r_ud[1]
+            Convert Black-Scholes / calibrate a binomial model'''
         self.r_ud    = [0., 0.]
-        self.r_ud[0] = math.exp(self.volatility*math.sqrt(self.maturity/self.n_periods))
-        self.r_ud[1] = 1.0/self.r_ud[0]
+        self.r_ud[0] = math.exp(self.volat*math.sqrt(self.matur/self.n_periods))
+        self.r_ud[1] = 1.0 / self.r_ud[0]
 
 
     def _risk_free_proba(self):
         '''Computes risk-free probability rfp=q from parameters u & d in r_ud'''
-        self.rfp = 0.
-        num    = math.exp((self.rate-self.dividend)*self.maturity/self.n_periods)
+        num    = math.exp((self.rate-self.dividend)*self.matur/self.n_periods)
         num    = num - self.r_ud[1]
         denom  = self.r_ud[0] - self.r_ud[1]
         self.rfp = num / denom
 
 
     def print_summary(self):
-        '''Prints summary parameters to std out'''
+        '''Prints summary parameters to stdout'''
         print(f'Initial price: {self.init}')
-        print(f'Maturity: {self.maturity} years / {self.n_periods} periods')
+        print(f'Maturity: {self.matur} years / {self.n_periods} periods')
         print(f'Risk-free rate: {100*self.rate:.2f}%')
         print(f'Dividend yield: {100*self.dividend:.2f}%')
-        print(f'Volatility: {100*self.volatility:.2f}%')
+        print(f'Volatility: {100*self.volat:.2f}%')
 
 
     def echo_model_parameters(self):
@@ -82,14 +82,16 @@ class SecurityParameters:
 
     def echo_risk_free_probability(self):
         '''prints risk-free probability q to stdout'''
-        print(f'q = {self.rfp:.5f} / 1-q = {1-self.rfp:.5f}\n')
+        print(f'q = {self.rfp:.5f} / 1-q = {1-self.rfp:.5f}')
+
 
 
 class Lattice:
     ''' Lattice superclass encapsulates parameters and functionality
-        for all lattices'''
+        common to lattices'''
     def __init__(self, size):
-        self.lattice = [[0 for x in range(size+1)] for y in range(size+1)]
+        # build a size x size list populated with 0
+        self.lattice = [['' for x in range(size+1)] for y in range(size+1)]
         self.size = size
 
 
@@ -106,7 +108,6 @@ class Lattice:
         '''Prints lattice to stdout'''
         print(f'{title} lattice:')
         print(pd.DataFrame(self.lattice).loc[::-1])
-        # print()
 
 
     def price_to_stdout(self):
@@ -116,22 +117,24 @@ class Lattice:
 
 
 class Options(Lattice):
-    ''' Options lattice / subclass of Lattice'''
+    ''' Options lattice / subclass of Lattice
+        underlying is lattice of either security or futures
+    '''
     def __init__(self, underlying, sec_par):
-        self.type  = TYPE
-        self.opt   = OPT
+        self.type   = TYPE
+        self.opt    = OPT
         self.strike = K
         self.expir  = EXPO
-        self.flags  = [1.0, 'E']
-        Lattice.__init__(self, self.expir)
+        super().__init__(self.expir)
         self._set_option_flags()
         self._build(underlying, sec_par)
 
 
-    def _build(self, underlying, sec_par):
+    def _build(self, underlying, sec_par): # build the lattice
         strike = self.strike
         flag   = self.flags[0]
         rfp    = sec_par.rfp
+
         for period in range(self.size, -1, -1):
             for option in range(period, -1, -1):
                 if period == self.size:
@@ -139,10 +142,11 @@ class Options(Lattice):
                     self.lattice[option][period] = max(flag*comp, 0.)
                 else:
                     num   = self.back_prop(option, period, rfp)
-                    denom = math.exp(sec_par.rate*sec_par.maturity/self.size)
+                    denom = math.exp(sec_par.rate*sec_par.matur/self.size)
                     ratio = num/denom
+                    # exercise value
+                    ex_val = strike - underlying.lattice[option][period]
 
-                    ex_val = strike - underlying.lattice[option][period] # exercise value
                     if self.flags[1] == 'E':
                         self.lattice[option][period] = ratio
                     else: # American option
@@ -153,6 +157,7 @@ class Options(Lattice):
 
     def _set_option_flags(self):
         '''Sets option flags for call/put & european/american'''
+        self.flags  = [1.0, 'E']
         if str.lower(self.opt) == 'put':
             self.flags[0] = -1.0
         elif str.lower(self.opt) != 'call':
@@ -168,21 +173,21 @@ class Options(Lattice):
 
 
     def print_summary(self):
-        '''Prints summary parameters to std out'''
+        '''Prints summary options parameters to std out'''
         print(f'{str.capitalize(self.type)} {self.opt} option')
-        print(f'Expiration: {self.expir}')
         print(f'Strike price={self.strike}')
-        print()
+        print(f'Expiration: {self.expir} periods\n')
+
 
 
 class Futures(Lattice):
     ''' Shares lattice / subclass of Lattice'''
     def __init__(self, underlying, sec_par):
-        Lattice.__init__(self, sec_par.n_periods)
+        super().__init__(sec_par.n_periods)
         self._build(underlying, sec_par)
 
 
-    def _build(self, underlying, sec_par):
+    def _build(self, underlying, sec_par): # build the lattice
         for period in range(self.size, -1, -1):
             for option in range(period, -1, -1):
                 if period == self.size: # F_T = S_T at maturity
@@ -196,11 +201,11 @@ class Futures(Lattice):
 class Shares(Lattice):
     ''' Shares lattice / subclass of Lattice'''
     def __init__(self, sec_par):
-        Lattice.__init__(self, sec_par.n_periods)
+        super().__init__(sec_par.n_periods)
         self._build(sec_par)
 
 
-    def _build(self, sec_par):
+    def _build(self, sec_par): # build the lattice
         self.lattice[0][0] = sec_par.init
         for period in range(1, self.size+1):
             for share in range(0, period+1):
@@ -214,7 +219,7 @@ class Shares(Lattice):
 
 
 def results_to_stdout():
-    '''Prints results to screen'''
+    '''Prints final results to screen'''
     sec.print_summary()
     print()
     options.print_summary()
@@ -222,29 +227,31 @@ def results_to_stdout():
 
 
 if __name__ == '__main__':
-    # Load parameters
+    # Load underlying security-related parameters
     sec = SecurityParameters()
 
+    # Display security-derived parameters (check)
     sec.echo_model_parameters()
     sec.echo_risk_free_probability()
 
-    # Security lattice
+    # Build lattice for underlying security
     shares = Shares(sec)
 
-    # Futures lattice
+    # Optionally build lattice for futures
     if FUTURES_FLAG:
         futures = Futures(shares, sec)
 
-    # Options lattice
-    if FUTURES_FLAG:
+    # Build lattice for options
+    if FUTURES_FLAG: # build options lattice from futures
         options = Options(futures, sec)
-    else:
+    else: # build options lattice from underlying security
         options = Options(shares, sec)
 
-    if LATTICE_FLAG:
+    if LATTICE_FLAG: # print lattices to sceen if flag set
         shares.lattice_to_stdout('Shares')
         if FUTURES_FLAG:
             futures.lattice_to_stdout('Futures')
         options.lattice_to_stdout('Options')
 
+    # Print final rresult to screen
     results_to_stdout()
