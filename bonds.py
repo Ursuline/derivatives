7ba2074efefa65943f7076d824b516c49578ddf4
Ursuline <3534155+Ursuline@users.noreply.github.com>
2021-01-20 15:24:55 +0100

    added bond forwards & futures

--- bonds.py
+++ bonds.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan 15 11:33:54 2021
+Created on Wed Jan 20 14:15:00 2021
 
 This follows the nomenclature of
 Financial Engineering and Risk Management Part I chapter 5
 Binomial lattice models of the short-rate;
-pricing fixed income derivatives, caplets, floorlets
+bond pricing
 
 on Coursera
 https://www.coursera.org/learn/financial-engineering-1/home/welcome
@@ -19,14 +19,10 @@ import options as op
 #### PARAMETERS ####
 # Lattice parameters:
 # TERM STRUCTURE
-TS_R00  = .05 # initial price
-TS_RUD  = [1.1, .9]
+TS_R00  = .06 # initial price
+TS_RUD  = [1.25, .9]
 TS_RNP  = [.5, .5] # risk-neutral probabilities
-TS_NPER = 10
-
-# ZCB PARAMETERS
-ZCB_FACE = 100 # Bond face value normally 100
-ZCB_NPER = 10
+TS_NPER = 5
 
 # Option parameters
 OP_K    = 84.0 # strike price
@@ -34,6 +30,15 @@ OPT     = 'call' # call or put option
 OP_TYPE = 'european' # option type: european or american
 OP_NPER = 4 # expiration - accomodates diff w/ #periods in lattice (EXPO<=N)
 
+# BOND parameters
+BOND_FACE = 100 # Bond face value normally 100
+BOND_NPER = 6
+BOND_COUPON = .1
+
+# Forward/future parameters
+FF_FACE = 100 # Bond face value normally 100
+FF_NPER = 4
+FF_TYPE = 'forward'
 
 ### Derivative selection to be set in main driver ###
 
@@ -86,46 +91,6 @@ class ShortRate(lt.Lattice):
         super().describe('Interest rate term structure', self.parameters, True)
 
 
-### ZERO-COUPON BONDS & ZCB OPTIONS ###
-class ZCBParameters(lt.Parameters):
-    '''Parameters for caplets/floorlets'''
-    def __init__(self, zcb_face, zcb_nper):
-        self.face = zcb_face
-        super().__init__(zcb_nper)
-
-
-    def describe(self):
-        ''' Prints summary parameters to stdout '''
-        print(f'Face value: {self.face}')
-        super().describe()
-
-
-
-class ZCB(lt.Lattice):
-    ''' Zero-coupon bond lattice '''
-
-    def __init__(self, zcb_parameters):
-        self.parameters = zcb_parameters
-        super().__init__(self.parameters.nperiods)
-
-
-    def build(self, sh_rate, term_par):
-        ''' build the zero-coupon bond lattice'''
-        for period in range(self.size, -1, -1):
-            for state in range(period, -1, -1):
-                if period == self.size:
-                    self.lattice[state][period] = self.parameters.face
-                else:
-                    num = self._back_prop(state, period, term_par.rnp)
-                    denom = 1 + sh_rate.lattice[state][period]
-                    self.lattice[state][period] = num / denom
-
-
-    def describe(self):
-        '''Self-descriptor'''
-        super().describe('Zero-coupon bond', self.parameters, False)
-
-
 
 class ZCBOptions(op.Options):
     ''' Options for Zero coupon bonds / Subclass of Options '''
@@ -160,13 +125,55 @@ class ZCBOptions(op.Options):
         '''Self-descriptor'''
         super().describe('Zero-coupon bond options', self.parameters, False)
 
+### BONDS
+class BondParameters(lt.Parameters):
+    '''Parameters for caplets/floorlets'''
+    def __init__(self, bond_face, bond_coupon, bond_nper):
+        self.face   = bond_face
+        self.coupon = bond_coupon
+        super().__init__(bond_nper)
+
+
+    def describe(self):
+        ''' Prints summary parameters to stdout '''
+        print(f'Face value: {self.face}')
+        print(f'Coupon value: {self.coupon}')
+        super().describe()
+
+
+
+class Bond(lt.Lattice):
+    def __init__(self, bond_parameters):
+        self.parameters = bond_parameters
+        super().__init__(self.parameters.nperiods)
+
+
+    def build(self, term_par, sh_rate):
+        ''' build the zero-coupon bond lattice'''
+        for period in range(self.size, -1, -1):
+            for state in range(period, -1, -1):
+                if period == self.size:
+                    self.lattice[state][period]  = self.parameters.face
+                    self.lattice[state][period] *= (1. + self.parameters.coupon)
+                else:
+                    num  = self._back_prop(state, period, term_par.rnp)
+                    denom = 1 + sh_rate.lattice[state][period]
+                    self.lattice[state][period]  = num / denom
+                    self.lattice[state][period] += self.parameters.face*self.parameters.coupon
+
+
+    def describe(self):
+        '''Self-descriptor'''
+        super().describe('Zero-coupon bond', self.parameters, False)
+
+
 
 ### BOND FORWARDS & FUTURES ###
 class BondFFParameters(lt.Parameters):
     '''Parameters for Bond forwards & futures'''
     def __init__(self, ff_type, ff_coupon, ff_nperiods):
-        if str.capitalize(ff_type) in ('forward', 'future'):
-            self.type    = str.capitalize(ff_type)
+        if str.lower(ff_type) in ('forward', 'future'):
+            self.type    = str.lower(ff_type)
         else:
             raise Exception(f'Invalid type: "{ff_type}"')
         self.coupon   = ff_coupon
@@ -178,69 +185,59 @@ class BondFFParameters(lt.Parameters):
         '''Self-descriptor'''
         print(f'Bond type: {self.type}')
         print(f'Coupon: {self.coupon}')
-        super().describe('Bond', self.parameters, False)
+        super().describe()
+
 
 
 class BondFF(lt.Lattice):
     ''' Lattice for Forward & Futures on bonds '''
     def __init__(self, bond_params):
         self.parameters = bond_params
-        super().__init__(self.parameters)
+        super().__init__(self.parameters.nperiods)
 
 
-    def build(self, ts_par, sh_rate, bond):
+    def build(self, ts_par, sh_rate, bond_l):
         for period in range(self.size, -1, -1):
             for state in range(period, -1, -1):
                 if period == self.size:
-                    self.lattice[state][period] = bond[state][period] - 100 * self.coupon
+                    self.lattice[state][period]  = bond_l.lattice[state][period]
+                    self.lattice[state][period] -= 100 * self.parameters.coupon
                 else:
-                    if self.type == 'forward':
-                        num   = ts_par.rnp[0] * self.lattice[state+1][period+1]
-                        num  += ts_par.rnp[1] * self.lattice[state][period+1]
-                        denom = sh_rate[state][period]
-                        self.lattice[state][period] = num / denom
-                    else: #futures
-                        num   = ts_par.rnp[0] * self.lattice[state+1][period+1]
-                        num  += ts_par.rnp[1] * self.lattice[state][period+1]
-                        denom = sh_rate[state][period]
-                        self.lattice[state][period] = num / denom
+                    num   = self._back_prop(state, period, ts_par.rnp)
+                    denom = 1.0
+                    if self.parameters.type == 'forward':
+                        denom += sh_rate.lattice[state][period]
+                    self.lattice[state][period] = num / denom
 
-
+    def describe(self):
+        '''Self-descriptor'''
+        super().describe(self.parameters.type, self.parameters, False)
 
 
 #### Driver ####
 if __name__ == '__main__':
     ## Derivative selection ##
-    # Set either of zcb, zcbopt
-    DERIVATIVE   = 'zcb'
+    # Set either of bondopt, bond, forward, future
+    OPTION   = False
     LATTICE_FLAG = True # print lattice to stdout
-    DERIVATIVE   = str.lower(DERIVATIVE)
 
     # Load underlying security-related parameters
     term_params = TermStructureParameters()
     short_rates = ShortRate(term_params)
     short_rates.display_lattice('Short-rate', True)
 
-    # Zero-coupon bonds and ZCB options
-    if DERIVATIVE in ('zcb', 'zcbopt'):
-        zcb = ZCB(ZCBParameters(ZCB_FACE, ZCB_NPER))
-        zcb.build(short_rates, term_params) # compute lattice
-        if LATTICE_FLAG:
-            zcb.display_lattice('ZCB')
+    bond = Bond(BondParameters(BOND_FACE, BOND_COUPON, BOND_NPER))
+    bond.build(term_params, short_rates) # compute lattice
+    bond.display_lattice('Bond')
+    bond.describe()
 
-        if DERIVATIVE == 'zcb':
-            short_rates.describe()
-        zcb.describe()
-
-        if DERIVATIVE == 'zcbopt': # ZCB options
-            option_params = op.OptionParameters(OPT, OP_TYPE, OP_K, OP_NPER)
-            zcb_opt       = ZCBOptions(option_params)
-            zcb_opt.build(zcb, short_rates)  # compute lattice
-            if LATTICE_FLAG:
-                zcb_opt.display_lattice('Zero option value')
-            short_rates.describe()
-            zcb_opt.describe()
+    ffbond = BondFF(BondFFParameters(FF_TYPE, BOND_COUPON, FF_NPER))
+    ffbond.build(term_params, short_rates, bond)
+    ffbond.display_lattice(str.capitalize(FF_TYPE))
+    ffbond.describe()
 
 
-    else:
-        raise Exception(f'Non-existent DERIVATIVE value: "{DERIVATIVE}"')
+
+
+
+
